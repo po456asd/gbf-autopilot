6795e4c8a3f37847e98445fa5c28c3599c96cd12
Izra Faturrahman <Frizz925@hotmail.com>
2017-06-23 07:49:17 +0700

    Added extension key generator script

--- keygen.py
+++ keygen.py
@@ -1,6 +1,10 @@
 from OpenSSL import crypto
 from base64 import b64encode
 import json
+import os
+
+dirname = os.path.dirname(__file__)
+manifestPath = os.path.join(dirname, 'extension/manifest.json')
 
 key = crypto.PKey()
 key.generate_key(crypto.TYPE_RSA, 2048)
@@ -8,9 +12,9 @@ dumped = crypto.dump_privatekey(crypto.FILETYPE_ASN1, key)
 encoded = b64encode(dumped)
 
 manifest = None
-with open('manifest.json') as f:
+with open(manifestPath) as f:
     manifest = json.load(f)
 
 manifest['key'] = encoded.decode('utf-8')
-with open('manifest.json', 'w') as f:
+with open(manifestPath, 'w') as f:
     json.dump(manifest, f, indent=2)
