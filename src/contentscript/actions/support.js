6a99cd55605b15aecd04aeb2d66ecade2d4a2bff
Izra Faturrahman <Frizz925@hotmail.com>
2017-06-27 05:23:15 +0700

    Added more weight for the base summon

--- src/contentscript/actions/support.js
+++ src/contentscript/actions/support.js
@@ -30,11 +30,11 @@ export default {
       const index = ids.indexOf(id);
       const base = index >= 0 ? ids.length - index : 0;
       // calculate from variables with least priority
-      const score = (5 * plus / 99) +   // 5%: start from the plus mark
-        (10 * Number(isMax)) +          // 10%: maybe useless but check if it's fully uncapped
-        (25 * level / 150) +            // 25%: prioritize based on the level
-        (50 * rank / 2) +               // 50%: next prioritize between none, MLB, and FLB
-        (100 * base / ids.length);      // 100%: the summon itself has the top priority
+      const score = (5 * plus / 99) +   // 5: start from the plus mark
+        (10 * Number(isMax)) +          // 10: maybe useless but check if it's fully uncapped
+        (25 * level / 150) +            // 25: prioritize based on the level
+        (50 * rank / 2) +               // 50: next prioritize between none, MLB, and FLB
+        (200 * base / ids.length);      // 200: the summon itself has the top priority
       
       if (score > max) {
         element = $el.find(".prt-supporter-info")[0];
